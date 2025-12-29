70c6352e4c3ffc83dbbd988caa0d563d10511bea
AmberFrost <kenny.mh.hui@outlook.com>
2025-12-30 01:00:04 +0800

    Improvements - Set maximum 2 sec timeout to prevent poor connection to webserver from launching the game - Allow server list to download once only

--- src/main/java/top/alazeprt/sls/util/HttpUtil.java
+++ src/main/java/top/alazeprt/sls/util/HttpUtil.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -14,10 +15,18 @@ import java.io.IOException;
 import static top.alazeprt.sls.config.SLSConfig.address;
 
 public class HttpUtil {
+    private static final int TIMEOUT_MS = 2000;
 
     public static JsonObject get() {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(TIMEOUT_MS)
+                .setConnectTimeout(TIMEOUT_MS)
+                .setSocketTimeout(TIMEOUT_MS)
+                .build();
+
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet(address);
+            httpGet.setConfig(requestConfig);
             HttpResponse response = httpClient.execute(httpGet);
             HttpEntity entity = response.getEntity();
             if (response.getStatusLine().getStatusCode() != 200) {
